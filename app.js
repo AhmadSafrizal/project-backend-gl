9189452347ac5161416ed35b3ddf060adf5b242a
AhmadSafrizal <ahmadsafrizal01@gmail.com>
2024-01-25 14:18:06 +0700

    git ignore update

--- app.js
+++ app.js
@@ -1,24 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const {
-  MongoClient,
-  ServerApiVersion
-} = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
-const produkRouter = require('./routes/produk');
-
-
-
-
-
-
+const produkRouter = require("./routes/produk");
 
 const app = express();
 app.use(router);
 app.listen(4000, () => {
   console.log("Server started on port 4000");
 });
-app.use('/produk', produkRouter);
-
+app.use("/produk", produkRouter);
 
 module.exports = app;
