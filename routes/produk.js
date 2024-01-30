e9914c6a7d0e809846f3180b86591ff08f959661
Yoga Ade Pangestu <yogadep@Yogas-MacBook-Air.local>
2024-01-30 10:20:35 +0700

    delete apiKey

--- routes/produk.js
+++ routes/produk.js
@@ -3,13 +3,12 @@ var router = express.Router();
 const produkController = require("../controller/produkController");
 var verif = require("../library/verif");
 
-router.get("/all", verif.cekAPI, produkController.getAllProduk);
-router.get("/kategori", verif.cekAPI, produkController.getAllKategori);
+router.get("/all",  produkController.getAllProduk);
+router.get("/kategori",  produkController.getAllKategori);
 router.get(
   "/kategori/:kategori_id",
-  verif.cekAPI,
   produkController.getProdukByKategoriId
 );
-router.get("/area/:area", verif.cekAPI, produkController.getProdukByArea);
+router.get("/area/:area", produkController.getProdukByArea);
 
 module.exports = router;
