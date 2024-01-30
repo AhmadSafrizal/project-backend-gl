48872502a7420060a6b76f54186423b539ee6006
Vrog <shawnmichaelazaria@gmail.com>
2024-01-30 11:30:31 +0700

    getuserrbyid

--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,7 @@ const usersController = require('../controller/usersController');
 
 
 router.get('/getAllUser', verif.cekAPI, usersController.getAllUser);
+router.get('/getUserById/:userId', usersController.getUserById);
 router.get('/getApiKey/:no_hp', usersController.getApiKey);
   
 module.exports = router;
